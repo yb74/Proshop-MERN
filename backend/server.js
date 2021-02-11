a5c6c965b3ad70b4a2ace446c72bca60c5d35500
younes <yb74@hotmail.fr>
2021-02-11 22:54:45 +0100

    Back-end User Authentication
    - Cleaning up the code by adding Controllers (functions moved from the Routes and put in a Controllers folder)
    - Setting User Authentication Endpoint
    - Generating a JSON Web Token
    - Implementing a custom Authentication Middleware
    - Saving the token in Postman
    -Implementing User Registration & Password Encryption

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import connectDB from './config/db.js'; // we put .js cos we use ES modules with node.js (backend)
-import productRoutes from './routes/productRoutes.js';
 import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js'; // we put .js cos we use ES modules with node.js (backend)
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json()); // allow to use Json data in body of request
+
 app.get('/', (req, res) => {
     res.send("API is running...");
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
