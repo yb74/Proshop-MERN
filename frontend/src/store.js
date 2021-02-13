1c2e3ecb5e5cd6a7cab3417262a3bb7fd72139c9
younes <yb74@hotmail.fr>
2021-02-13 23:52:49 +0100

    Checkout Process - Part 1
    - Creating the shipping screen & saving the address
    - Implementing the Checkout Steps Component
    - Creating the payment screen & saving the payment method
    - Creating the place order screen
    - Setting the Order Controller & Order routes
    - Implementing the unction to create an order and send it to the database

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import {productListReducer, productDetailsReducer} from './reducers/productReducers';
 import {cartReducer} from './reducers/cartReducers';
 import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers';
+import {orderCreateReducer} from './reducers/orderReducers';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -12,7 +13,8 @@ const reducer = combineReducers({
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
@@ -23,8 +25,15 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
 ? JSON.parse(localStorage.getItem('userInfo')) 
 : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') 
+? JSON.parse(localStorage.getItem('shippingAddress')) 
+: {};
+
 const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
+    cart: {
+        cartItems: cartItemsFromStorage, 
+        shippingAddress: shippingAddressFromStorage
+    },
     userLogin: {userInfo: userInfoFromStorage}
 };
 
