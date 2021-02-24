c5195e5c766f999f698d2fe9dba73ca19e946e07
younes <yb74@hotmail.fr>
2021-02-24 15:50:00 +0100

    Additional Features
    - Implementing Product Search Bar
    - Adding Product Pagination
    - Implementing Top Rated Products Carousel
    - Implementing Helmet npm package to manage metadata, titles... (Custom page Titles, Meta...)

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,9 +5,20 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1;
 
-    res.json(products);
+    const keyword = req.query.keyword ? {
+        name: {
+            $regex: req.query.keyword, // we use regex (not directly name: req.query.keyword) so that we have product suggestion when we start typing the name of a product (i.e: "Iph" suggest "Iphone")
+            $options: 'i' // case insensitive
+        }
+    } : {};
+
+    const count =  await Product.countDocuments({...keyword});
+    const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page - 1));
+
+    res.json({products, page, pages: Math.ceil(count / pageSize)});
 });
 
 // @desc Fetch single product
@@ -129,4 +140,13 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 });
 
-export {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview};
+// @desc Get top rated products 
+// @route GET /api/products/top
+// @access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({rating: -1}).limit(3);
+
+    res.json(products);
+});
+
+export {getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts};
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -6,6 +6,7 @@ import Rating from '../components/Rating';
 import {listProductDetails, createProductReview} from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Meta from '../components/Meta';
 import {PRODUCT_CREATE_REVIEW_RESET} from '../constants/productConstants';
 
 const ProductScreen = ({history, match}) => {
@@ -53,6 +54,7 @@ const ProductScreen = ({history, match}) => {
             </Link>
             { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
                 <>
+                    <Meta title={product.name} />
                     <Row>
                         <Col md={6}>
                             <Image src={product.image} alt={product.name} fluid />
